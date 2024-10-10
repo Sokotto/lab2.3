e94c6a4c2c199489cff017d6bf73c777fa93e88b
Xenzey <Xenzey@209-3>
2024-10-10 11:31:09 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+    Console.WriteLine("Введите длыны сторон треугольника a, b, c: ");
+    double a = double.Parse(Console.ReadLine());
+    double b = double.Parse(Console.ReadLine());
+    double c = double.Parse(Console.ReadLine());
+    double minSide = Math.Min(a,Math.Min(b,c));
+    double sqrArea = minSide + minSide;
+    double semiPerimeter = (a + b + c) / 2;
+    double triangleArea = Math.Sqrt(semiPerimeter *(semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+    Console.WriteLine($"Площадь квадрата: {sqrArea}");
+    Console.WriteLine($"Площадь треугольника: {triangleArea}");
+    if (sqrArea > triangleArea)
+    {
+        Console.WriteLine("Площадь квадрата больше площади треугольника.");
+    }
+    else if (sqrArea < triangleArea)
+    {
+        Console.WriteLine("Площадь треугольника больше площади квадрата.");
+    }
+    else
+    {
+        Console.WriteLine("Площади квадрата и треугольника равны.");
+    }
+}
+catch
+{
+    Console.WriteLine("Введите правильно!");
+}
